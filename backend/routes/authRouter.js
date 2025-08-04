bd25fa112a02b52400ef94c8a4d7daa9b11d332b
Gabriela <gabrielacunarro@gmail.com>
2025-08-04 10:20:18 -0400

    Agregada ruta GET para reset de contraseña

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const { login, recuperarPassword, resetPassword } = require('../controllers/authController.js');
 
 router.post('/login', login);
 router.post('/reset', recuperarPassword);
 router.post('/reset/:token', resetPassword);
 router.post('/forgot', recuperarPassword);
-
+router.get('/reset', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/reset-password.html'));
+});
 
 module.exports = router;
